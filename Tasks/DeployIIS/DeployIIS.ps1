dae1eb884a3abf89f1b7ed7d0a3481ce33a13770
Christopher Woolum <Christopher.Woolum@technossus.com>
2016-04-01 13:06:47 -0700

    add retry capability

--- Tasks/DeployIIS/DeployIIS.ps1
+++ Tasks/DeployIIS/DeployIIS.ps1
@@ -54,7 +54,7 @@ invoke-command -session $session -scriptblock {
     } else {      
       $physicalPath = $overridePath    
     }
-
+    
     try
     {
         if((Get-Website -Name $name))
@@ -71,55 +71,75 @@ invoke-command -session $session -scriptblock {
     "Creating App Pool"
     # Create Application Pool
     
-     try {
+    try {
      	$poolCreated = Get-WebAppPoolState $name –errorvariable myerrorvariable
-	if($poolCreated)
-	{
-        	Remove-WebAppPool $name
-     	}
-     }	
+        if($poolCreated)
+        {
+                Remove-WebAppPool $name
+        }
+    }	
 	catch
 	{
 		"App pool does not exist"
 	}
     
-     $appPool = New-WebAppPool -Name $name
-     Set-ItemProperty IIS:\AppPools\$name -name processModel -value @{userName="$serviceAccountName";identitytype=3}
-     Set-ItemProperty IIS:\AppPools\$name managedRuntimeVersion v4.0    
-    
-    Write-Host "32-bit Mode: $enableThirtyTwoBit"
-    Set-ItemProperty IIS:\AppPools\$name -name enable32BitAppOnWin64 -Value "$enableThirtyTwoBit"
-   
-    "Creating Folder"
-    # Create Folder for the website
-    if(!(Test-Path $physicalPath)) {
-     md $physicalPath
-    }
-    else {
-     Remove-Item "$physicalPath\*" -recurse -Force
-    }
- 
-    $site = Get-WebSite | where { $_.Name -eq $name }
-    if($site -eq $null)
-    {
-     Write-Host "Creating site: $name $physicalPath"   
-     New-WebSite -PhysicalPath $physicalPath -Name $name -HostHeader $hostName -ApplicationPool $name  
-    }
+    $retries = 5
+    $retrycount = 0
+    $completed = $false
 
-    if($certificateThumbprint){
-        Write-Host "Using SSL"
-        Write-Host "IIS:\SslBindings\*!443!$hostName"
-        New-WebBinding -Name $name -Protocol "https" -Port 443 -HostHeader $hostName -SslFlags 1
-        try{
-        New-Item -Path "IIS:\SslBindings\*!443!$hostName" -Thumbprint $certificateThumbprint -SSLFlags 1
+     while (-not $completed) {
+        try {
+            New-WebAppPool -Name $name
+            Set-ItemProperty IIS:\AppPools\$name -name processModel -value @{userName="$serviceAccountName";identitytype=3}
+            Set-ItemProperty IIS:\AppPools\$name managedRuntimeVersion v4.0    
+            
+            Write-Host "32-bit Mode: $enableThirtyTwoBit"
+            Set-ItemProperty IIS:\AppPools\$name -name enable32BitAppOnWin64 -Value "$enableThirtyTwoBit"
+        
+            "Creating Folder"
+            # Create Folder for the website
+            if(!(Test-Path $physicalPath)) {
+                md $physicalPath
+            }
+            else {
+                Remove-Item "$physicalPath\*" -recurse -Force
+            }
+        
+            $site = Get-WebSite | where { $_.Name -eq $name }
+            if($site -eq $null)
+            {
+                Write-Host "Creating site: $name $physicalPath"   
+                New-WebSite -PhysicalPath $physicalPath -Name $name -HostHeader $hostName -ApplicationPool $name  -Force
+            }
+
+            if($certificateThumbprint){
+                Write-Host "Using SSL"
+                Write-Host "IIS:\SslBindings\*!443!$hostName"
+                New-WebBinding -Name $name -Protocol "https" -Port 443 -HostHeader $hostName -SslFlags 1
+                
+                try{
+                    New-Item -Path "IIS:\SslBindings\*!443!$hostName" -Thumbprint $certificateThumbprint -SSLFlags 1
+                }
+                catch{
+                    Write-Host "Certificate does not exist"
+                }
+            } else{
+                Write-Host "Not using SSL"
+            } 
+            
+            $completed = $true
+            
+        } catch {                
+            if ($retrycount -ge $retries) {
+                Write-Verbose ("Command [{0}] failed the maximum number of {1} times." -f $command, $retrycount)
+                throw
+            } else {
+                Write-Verbose ("Command [{0}] failed. Retrying in {1} seconds." -f $command, 15)
+                Start-Sleep 15
+                $retrycount++
+            }
         }
-        catch{
-
-        }
-    } else{
-     Write-Host "Not using SSL"
-    }
-
+     }
  
 } -ArgumentList $name, $hostName, $serviceAccountName, $certificateThumbprint, $overridePath, $enableThirtyTwoBit
  
